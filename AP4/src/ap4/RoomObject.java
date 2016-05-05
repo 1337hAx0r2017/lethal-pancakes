6a4c89a166856a1ba09649cb37a7b6d0cc109a01
kardzhalao.2017 <kardzhalao.2017@KHS-C136-24765.kenston.local>
2016-05-05 09:10:32 -0400

    testing

--- AP4/src/ap4/RoomObject.java
+++ AP4/src/ap4/RoomObject.java
@@ -7,42 +7,23 @@ public class RoomObject {
     float x;
     float y;
     
-    //consider 3dmondel vs images
+    //consider 3dmodel vs images on gui
     
    RoomObject(float x, float y)
    {
        this.x = x;
        this.y = y;
-       
-            
-       
-         
-       
-       
-       
+      
    }
      
-   
-   
-   
    void movement()
-   {
-       
-       
-       
-       
+   {   
        
    }
-   
-   
-   
+  
    void paint(Graphics g)
    {
       //paint the images we put in 
-       
-       
-       
-       
-       
+      
    }
 }
