7cdf090518d23ae90f8d1e06bb545b5cbb120aaa
ed.mason <ed.mason@KHS-C136-24651P.kenston.local>
2016-05-06 08:28:45 -0400

    Framework

--- AP4/src/gui/GamePanel.java
+++ AP4/src/gui/GamePanel.java
@@ -15,60 +15,14 @@ import javax.swing.JPanel;
  *
  * @author ed.mason
  */
-public class GamePanel extends JPanel {
+public class GamePanel extends GeneralGamePanel {
     Game game;
-    boolean running;
-    double updateTime;
-    double drawTime;
-    double cycleTime;
-    Thread loop;
-    final float hertz = 30;
-    final float seconds = 1 / hertz;
-    final long nanoseconds = (int)(1000000000 * seconds);
     
     public GamePanel(Game game)
     {
         this.game = game;
-        loop = new Thread(new GameLoop());
     }
     
-    public void start()
-    {
-        running = true;
-        loop.start();
-    }
-    
-    public void stop()
-    {
-        running = false;
-    }
-    
-    class GameLoop implements Runnable
-    {
-
-        @Override
-        public void run() {
-            try {
-                while(running)
-                {
-                    long startUpdateTime = System.nanoTime();
-                    update(seconds);
-                    long endUpdateTime = System.nanoTime();
-                    repaint();
-                    long endDrawTime = System.nanoTime();
-                    int sleepTime = (int)(nanoseconds - (endDrawTime - startUpdateTime));
-                    Thread.sleep(sleepTime / 1000000, sleepTime % 1000000);
-                    long endSleepTime = System.nanoTime();
-                    updateTime = (endUpdateTime - startUpdateTime) / 1000000000.0;
-                    drawTime = (endDrawTime - endUpdateTime) / 1000000000.0;
-                    cycleTime = (endSleepTime - startUpdateTime) / 1000000000.0;
-                }
-            } catch (InterruptedException ex) {
-                Logger.getLogger(GamePanel.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        
-    }
     
     public void update(float time)
     {
@@ -84,8 +38,8 @@ public class GamePanel extends JPanel {
         
         //game.camera.endDraw(g);
         
-        g.drawString("Update Time: " + ((int)(updateTime * 1000000)) + "ms (" +  ((int)(1.0/updateTime)) +"Hz)",0, 20);
-        g.drawString("Draw Time: " + ((int)(drawTime * 1000000)) + "ms (" +  ((int)(1.0/drawTime)) +"Hz)",0, 40);
-        g.drawString("Cycle Time: " + ((int)(cycleTime * 1000000)) + "ms (" +  ((int)(1.0/cycleTime)) +"Hz)",0, 60);
+        g.drawString("Update Time: " + ((int)(getUpdateTime() * 1000000)) + "ms (" +  ((int)(1.0/getUpdateTime())) +"Hz)",0, 20);
+        g.drawString("Draw Time: " + ((int)(getDrawTime() * 1000000)) + "ms (" +  ((int)(1.0/getDrawTime())) +"Hz)",0, 40);
+        g.drawString("Cycle Time: " + ((int)(getCycleTime() * 1000000)) + "ms (" +  ((int)(1.0/getCycleTime())) +"Hz)",0, 60);
     }
 }
