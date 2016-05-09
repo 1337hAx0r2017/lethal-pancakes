7b9e445678911d98251727d4d01d0a5ac1917bdf
kardzhalao.2017 <kardzhalao.2017@KHS-C136-24765.kenston.local>
2016-05-09 08:12:59 -0400

    Merge origin/dev into dev
    Conflicts:
    	AP4/src/ap4/Game.java

--- AP4/src/ap4/RoomObject.java
+++ AP4/src/ap4/RoomObject.java
@@ -3,14 +3,26 @@ package ap4;
 import java.awt.Graphics;
 
 public abstract class RoomObject {  
-   public float x;
-   public float y;     
+  
+    public float x;
+    public float y;     
+   
    RoomObject(float x, float y)// consider later how graphics will be added in
    { 
        this.x = x;
        this.y = y;
       //consider 3dmodel vs images on gui    
    } 
+   
+   public float distanceTo(float x, float y)
+   {
+   
+   }
+   public float distanceTo(RoomObject object)
+   {
+     return distanceTo(object.x, object.y);   
+   }
+
    abstract void draw(Graphics g);
    //paint the images we put in   
    abstract void update(Game game, float time);//will be different in object3d and image object       
