2979e35a146a2f1c32b4047d7f4058fc732cbda2
ParsonsW.2018 <ParsonsW.2018@KHS-C136-24762.kenston.local>
2016-05-20 08:25:30 -0400

    adding tiles

---
+++ AP4/src/ap4/rooms/Room16.java
@@ -0,0 +1,21 @@
+package ap4.rooms;
+
+import ap4.map.Room;
+
+public class Room16 extends Room {
+    
+    public Room16()
+    {
+        super();
+        exits[0] = true; //north
+        exits[1] = true; //east
+        exits[2] = true; //south
+        exits[3] = true; //west
+       
+        // First make all tiles air tiles (they have floor though) via blankTiles(), called in super class
+        // Now add other tiles (rocks, walls, etc)
+       
+        // Finalize tiles
+        finalizeTiles();
+    }
+}
